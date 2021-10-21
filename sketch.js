1661e1310648fd9b507491d2605e0ac28349464e
MaddieMcNamara <91571511+MaddieMcNamara@users.noreply.github.com>
2021-10-20 22:27:14 -0700

    update

--- sketch.js
+++ sketch.js
@@ -7,27 +7,63 @@ let dogs = [{
 }];
 
 let randomIndex;
+let animating = false;
+// let counter = 0;
 
 function setup() {
   createCanvas(600, 600);
   background(200);
   textSize(32);
 
+// setTimeout(changeBackground, 1000);
 
 }
 
 function draw() {
 
+if(animating == true) {
+ellipse(random(width), random(height), random(50, 200));
+
+}
+
+}
+// function changeBackground() {
+//
+// if (counter <= 5) {
+// counter++;
+// console.log(counter)
+//
+//
+//   background(random(255), random(255), random(255));
+//   settimeout(changeBackground, 1000);
+// }
+function randomizer() {
+
+  animating = false;
+  if (dogs[0]); {
+
+    background(random(200, 255));
+    randomIndex = int(random(dogs.length));
+    text(`${dogs[randomIndex].name}'s favorite color is
+    ${dogs[randomIndex].color}`, 50, 50);
 
 
+    // text(dogs[randomIndex].name + "'s favorite color is" 50, 50);
+    // dogs[randomIndex].color, 50, 50);
+
+
+    dogs.splice(randomIndex, 1);
+
+  }
 
 }
 
 function mousePressed() {
+  animating = true;
 
-  background(random(200, 255));
-  randomIndex = int(random(dogs.length));
-  text(dogs[randomIndex].name, 50, 50);
-  dogs.splice(randomIndex, 1);
+setTimeout(randomizer, 2000);
+
+
+
 
 }
